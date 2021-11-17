e758629ab45e3c080b9ece309c00b01061b9e9ba
Alexandre Capt <acapt@adobe.com>
2021-11-17 08:54:35 +0100

    chore: quotes consistency

--- tools/product/picker.js
+++ tools/product/picker.js
@@ -40,25 +40,25 @@ function handleAddValue(event) {
   var sku = event.detail.value;
 
   // TODO find data
-  const name = "Product Name goes here";
-  const imgSrc = "http://mystage1-amspro120.amscommerce.cloud/media/catalog/product/cache/8735dd21982cf027014173d1affcf80c/v/d/vd09-pe_main_4.jpg";
+  const name = 'Product Name goes here';
+  const imgSrc = 'http://mystage1-amspro120.amscommerce.cloud/media/catalog/product/cache/8735dd21982cf027014173d1affcf80c/v/d/vd09-pe_main_4.jpg';
 
   addToSelection(sku, {
     name,
     imgSrc
   });
 
-  var products = document.querySelector("#products");
-  products.style.display = "block";
+  var products = document.querySelector('#products');
+  products.style.display = 'block';
 
-  var tableBody = products.querySelector("table tbody");
-  var row = createTag("tr", { id: "p-" + sku, class: "spectrum-Table-row" });
-  var cellSku = createTag("td", {class: "spectrum-Table-cell"});
+  var tableBody = products.querySelector('table tbody');
+  var row = createTag('tr', { id: 'p-' + sku, class: 'spectrum-Table-row' });
+  var cellSku = createTag('td', {class: 'spectrum-Table-cell'});
   cellSku.innerHTML = sku;
-  var cellName = createTag("td",  { class: "spectrum-Table-cell" });
+  var cellName = createTag('td',  { class: 'spectrum-Table-cell' });
   cellName.innerHTML = name;
-  var cellImage = createTag("td",  { class: "spectrum-Table-cell" });
-  var img = createTag("img", { src: imgSrc, class: "image-preview" });
+  var cellImage = createTag('td',  { class: 'spectrum-Table-cell' });
+  var img = createTag('img', { src: imgSrc, class: 'image-preview' });
   cellImage.appendChild(img);
   row.appendChild(cellSku);
   row.appendChild(cellName);
@@ -71,18 +71,18 @@ function handleRemoveValue(event) {
 
   removeFromSelection(sku);
 
-  var products = document.querySelector("#products");
-  var productRow = products.querySelector("table tbody #p-" + sku);
+  var products = document.querySelector('#products');
+  var productRow = products.querySelector('table tbody #p-' + sku);
   productRow.remove();
 }
 
 
-// document.querySelector("#products").addEventListener('cif:update-value', () => {
-//     console.log("Product - change")
+// document.querySelector('#products').addEventListener('cif:update-value', () => {
+//     console.log('Product - change')
 // });
 
 export function init() {
-  document.querySelector("#products").addEventListener('cif:add-value', handleAddValue);
-  document.querySelector("#products").addEventListener('cif:remove-value', handleRemoveValue);
-  copyButton = document.querySelector("#copy");
+  document.querySelector('#products').addEventListener('cif:add-value', handleAddValue);
+  document.querySelector('#products').addEventListener('cif:remove-value', handleRemoveValue);
+  copyButton = document.querySelector('#copy');
 }
