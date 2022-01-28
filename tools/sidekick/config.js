b54c7c45861954d0469ac7045363bf4a250ac780
Alexandre Capt <acapt@adobe.com>
2022-01-28 11:39:29 +0100

    chore: sidekick used for the demo

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -16,7 +16,52 @@
   window.hlx.initSidekick({
       project: 'helix-chefkoch',
       hlx3: true,
-      plugins: [
+      plugins: [{
+        id: 'edit',
+        condition: () => false,
+      }, {
+        id: 'preview',
+        condition: () => false,
+      }, {
+        id: 'publish',
+        condition: () => false,
+      }, {
+        id: 'live',
+        condition: () => false,
+      }, {
+        id: 'edit2',
+        condition: (sidekick) => !sidekick.isEditor() && sidekick.isHelix()
+            && sidekick.status.edit && sidekick.status.edit.url,
+        button: {
+            text: 'Edit',
+            action: async (e, sk) => {
+                const { config, status } = sk;
+                // const editUrl = status.edit && status.edit.url;
+                const editUrl = 'https://docs.google.com/document/d/1phTkZrSJdw6S5lW716mrgXyPrRhIm-PXXN-FBu60eYg/edit';
+                
+                window.open(editUrl, `hlx-sk-edit--${config.owner}/${config.repo}/${config.ref}${status.webPath}`);
+            },
+        }
+    }, {
+          // CUSTOM EDIT / PREVIEW
+        id: 'preview2',
+        condition: (sidekick) => sidekick.isEditor() || sidekick.isHelix(),
+        button: {
+            text: 'Preview',
+            action: async (evt, sk) => {
+                sk.showModal('Please wait …', true);
+                const res = await sk.update();
+                const { path } = res;
+                let p = path.replace('/venia-helix', '');
+                const res2 = await fetch(`https://${sk.config.innerHost}/url-mappings.json`);
+                const json = await res2.json();
+                const mapping = json.data.find(e => e.hlx === p);
+                window.open(`http://localhost:10000${mapping ? mapping.venia : p}`);
+                sk.hideModal();
+            },
+            isPressed: (sidekick) => sidekick.isInner(),
+            },
+        },
           // TEMPLATES --------------------------------------------------------------------
           {
               id: 'product-picker',
